de07cc8bc2678d7eb65086ee6b19f9f58825e64d
I Wayan Dharmana <wdharmana@gmail.com>
2017-10-11 22:34:07 +0800

    finish basic ui main & result

--- app/src/main/java/com/android/cinematix/MainActivity.java
+++ app/src/main/java/com/android/cinematix/MainActivity.java
@@ -1,13 +1,28 @@
 package com.android.cinematix;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBook = (Button) findViewById(R.id.btn_book);
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
